8b6a56c4a833453912b2c34aa63f0f41d4953ecb
wenluzhi <851673040@qq.com>
2015-12-08 10:31:32 +0800

    d

---
+++ Assets/Scripts/MainScripts.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class HomePagePanel{
+	public GameObject mainHomePageBg;
+}
+
+
+public class MainScripts : MonoBehaviour {
+
+	[SerializeField] private HomePagePanel homePagePanel;
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
